5d9cbef08ed453f1c45c71497bef57d2af33e1b3
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2024-02-25 20:53:10 -0600

    wip

--- keyboards/keychron/q6/iso_encoder/keymaps/siterelenby/keymap.c
+++ keyboards/keychron/q6/iso_encoder/keymaps/siterelenby/keymap.c
@@ -15,10 +15,31 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "keychron_common.h"
+//#include "keychron_common.h"
+
+// copied keychron_common without the custom defs in order to add them
+#include "keychron_common_nocustom.h"
+
+// TODO: make mac mode switch do something useful instead
+// otherwise copied from keychron_common.h
+
+enum custom_keycodes {
+    KC_LOPTN = QK_KB_2, // TECH DEBT: Starts at QK_KB_2 to maintain ordering with VIA definitions. See #19884. Revert to QK_KB_0 when VIA catches up with QMK.
+    KC_ROPTN,
+    KC_LCMMD,
+    KC_RCMMD,
+    KC_SIRI,
+// useless mac ones above here, TODO: remove/repurpose
+    KC_TASK_VIEW,
+    KC_FILE_EXPLORER,
+    KC_SCREEN_SHOT,
+//  KC_CORTANA // no thanks, lol
+    ENTCLK = SAFE_RANGE,
+    DWNCLK,// = SAFE_RANGE + 1,
+};
+
 
 // clang-format off
-
 enum layers{
   MAC_BASE,
   MAC_FN,
@@ -27,6 +48,31 @@ enum layers{
   WIN_FNC
 };
 
+// todo: tri layer for ctrl/fn?
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(60, 255, 255, 255); // assuming caps lock is at LED #60
+    } else {
+        //RGB_MATRIX_INDICATOR_SET_COLOR(60, 0, 0, 0);
+    }
+    if (host_keyboard_led_state().num_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(37, 255, 255, 255); // assuming num lock is at LED #37
+    } else {
+        //RGB_MATRIX_INDICATOR_SET_COLOR(37, 0, 0, 0);
+    }
+    return false;
+}
+
+// Knob: Scroll/middle click
+// RGB: Mode
+// Fn:
+//     Knob: Volume/mute
+//     RGB: toggle
+//     Enter: Enter + leftclick
+// Fn+Ctrl:
+//     Knob: RGB brightness/toggle
+//     RGB key: previous mode
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_iso_110(
@@ -44,19 +90,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
     [WIN_BASE] = LAYOUT_iso_110(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_BTN3,    KC_PSCR,  KC_SCRL,  RGB_MOD,  KC_BTN3,  KC_BTN4,  KC_BTN5,  _______,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_BTN3,    KC_PSCR,  KC_SCRL,  RGB_MOD,  KC_BTN3,  KC_BTN4,  KC_BTN5,  ENTCLK,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
     [WIN_FN] = LAYOUT_iso_110(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    _______,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    _______,  _______,  RGB_TOG,  _______,  _______,  ENTCLK,  DWNCLK,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,                                  _______,  _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    ENTCLK,                                   _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
-        MO(WIN_FNC), _______,  _______,                             _______,                                _______,  _______,  _______,    MO(WIN_FNC), _______, _______,  _______,  _______,            _______,  _______),
+        MO(WIN_FNC), _______,  _______,                             _______,                                _______,  _______,  _______,    MO(WIN_FNC), _______, DWNCLK,   _______,  _______,            _______,  _______),
     [WIN_FNC] = LAYOUT_iso_110(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    RGB_TOG,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
@@ -65,6 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
 };
+//TODO: fn/ctrl tri layer
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -87,5 +134,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
 
-    return true;
+    switch (keycode) {
+        case ENTCLK:
+            if (record->event.pressed) {
+                // when keycode ENTCLK is pressed
+                SEND_STRING(SS_TAP(X_ENT) SS_DELAY(100) SS_TAP(X_MS_BTN1));
+            } else {
+                // when keycode ENTCLK is released
+            }
+        break;
+        case DWNCLK:
+            if  (record->event.pressed) {
+                // when keycode DWNCLK is pressed
+                //SEND_STRING(SS_TAP(X_DOWN) SS_DELAY(100) SS_TAP(X_MS_BTN1));
+
+                //SEND_STRING(SS_TAP(X_DOWN));
+
+                //tap_code(KC_DOWN);
+                //tap_code(KC_MS_BTN1);
+
+                tap_code_delay(KC_A, 100);
+                //tap_code_delay(KC_DOWN, 100);
+                tap_code_delay(KC_MS_BTN1, 100);
+            } else {
+                // when keycode DWNCLK is released
+                //SEND_STRING(SS_TAP(X_MS_BTN1));
+            }
+        break;
+        }
+        return true;
 }
